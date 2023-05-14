c1796eabd8a6c220a79325421775020acf4ef416
Kennedy <dkennyvs@gmail.com>
2023-05-14 20:02:55 -0300

    Leia 4 números e imprima a média aritimética

--- Lista de exercicio 1 - variaveis/exercicio_03.py
+++ Lista de exercicio 1 - variaveis/exercicio_03.py
@@ -1,12 +1,12 @@
 # Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
 lista_notas = []
-media = soma = 0
+media_notas = soma = 0
 
 for i in range(4):
 	entrada_notas = float(input(f'Digite a {i+1}º nota: '))
 	lista_notas.append(entrada_notas)
 	soma += entrada_notas
-	media = soma / len(lista_notas)
+	media_notas = soma / len(lista_notas)
 
-print(f'A média é: {media:.2f}')
+print(f'A média é: {media_notas:.2f}')
