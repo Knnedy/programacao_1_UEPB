1bcfc9838dcb3c798009cea2e656d2bf649e1c65
Kennedy <dkennyvs@gmail.com>
2023-05-14 21:30:59 -0300

    Conversão de Farenheit para Celsius

--- Lista de exercicio 1 - variaveis/exercicio_08.py
+++ Lista de exercicio 1 - variaveis/exercicio_08.py
@@ -1,5 +1,5 @@
-# Faça um Programa que peça a temperatura em graus Farenheit,transforme
-# e mostre a temperatura em graus Celsius. C = (5 * (F-32) / 9)
+# Faça um Programa que peça a temperatura em graus Farenheit,transforme e
+# mostre a temperatura em graus Celsius. C = (5 * (F-32) / 9)
 
 def obter_temperatura_farenheit():
     f = float(input('Digite a temperatura em Farenheit: '))
