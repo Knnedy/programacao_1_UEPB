15cbd5a185fd8a454836eb6e302e85be27eb6fd3
Kennedy <dkennyvs@gmail.com>
2023-05-19 13:38:25 -0300

    Faça um Programa que peça dois números e imprima a soma.

--- Lista de exercicio 1 - variaveis/exercicio_02.py
+++ Lista de exercicio 1 - variaveis/exercicio_02.py
@@ -1,10 +1,32 @@
 # Faça um Programa que peça dois números e imprima a soma
 
-lista_numeros = []
-soma = 0
-for i in range(2):
-	num = int(input('Digite um número: '))
-	lista_numeros.append(num)
-	soma += num
+from colorama import Fore, Style
 
-print(f'A soma de {lista_numeros[0]} e {lista_numeros[1]} é:', soma)
+
+def calcular_soma_numeros():
+    lista_numerica = []
+    soma = 0
+    
+    for i in range(2):
+        while True:
+            try:
+                num = int(input('Digite o número: '))
+                lista_numerica.append(num)
+                soma += num
+                break
+            except ValueError:
+                print(f'{Fore.RED}{Style.BRIGHT}'
+                      f'Entrada inválida. Digite um número inteiro.'
+                      f'{Fore.RED}{Style.BRIGHT}')
+    
+    return lista_numerica, soma
+
+
+def main():
+    lista_numerica, soma = calcular_soma_numeros()
+    print(f'A Soma de {lista_numerica[0]} e {lista_numerica[1]} é: {soma}')
+  
+        
+if __name__ == '__main__':
+    main()
+    
