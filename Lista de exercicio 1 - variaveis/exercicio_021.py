d3f97dbbecff9fcec9b312548b9e9dd72b66665b
Kennedy <dkennyvs@gmail.com>
2023-05-18 22:18:00 -0300

    Calculo de salário com descontos...

--- Lista de exercicio 1 - variaveis/exercicio_021.py
+++ Lista de exercicio 1 - variaveis/exercicio_021.py
@@ -12,3 +12,63 @@
 #         Sindicato ( 5%) : R$ <br>
 #         Salário Liquido : R$ <br>
 # Obs.: Salário Bruto - Descontos = Salário Líquido.
+
+from colorama import Fore, Style
+
+
+def calcular_salario_bruto(sal_hora, hrs_mensais):
+    return sal_hora * hrs_mensais
+    
+
+def calcular_imposto_renda(salario_bruto):
+    return salario_bruto * 0.11
+
+
+def calcular_desconto_sindicato(salario_bruto):
+    return salario_bruto * 0.05
+    
+
+def calcular_inss(salario_bruto):
+    return salario_bruto * 0.08
+
+
+def calcular_salario_liquido(salario_bruto, imposto_renda, inss, sindicato):
+    return salario_bruto - imposto_renda - inss - sindicato
+
+
+def main():
+    while True:
+        try:
+            salario_hora = float(input('Salário por hora: R$'))
+            horas_mensais = float(input('Horas mensais: '))
+        
+            if salario_hora <= 0 and horas_mensais <= 0:
+                raise ValueError(f'{Fore.RED}{Style.BRIGHT}O salário por hora e as horas '
+                                 f'mensais devem ser maiores que 0{Fore.RED}{Style.BRIGHT}')
+            else:
+                salario_bruto = calcular_salario_bruto(salario_hora, horas_mensais)
+                desconto_imposto_renda = calcular_imposto_renda(salario_bruto)
+                desconto_inss = calcular_inss(salario_bruto)
+                desconto_sindicato = calcular_desconto_sindicato(salario_bruto)
+                salario_liquido = calcular_salario_liquido(salario_bruto,
+                                                           desconto_imposto_renda,
+                                                           desconto_inss,
+                                                           desconto_sindicato)
+                
+                print(f'Salário Bruto         : R${salario_bruto:.2f}')
+                print(f'IR (11%)              : R${desconto_imposto_renda:.2f}')
+                print(f'INSS (8%)             : R${desconto_inss:.2f}')
+                print(f'Sindicato (5%)        : R${desconto_sindicato:.2f}')
+                print('----------------------------------')
+                print(f'SALÁRIO LIQUIDO       : R${salario_liquido:.2f}')
+                break
+            
+        except ValueError as error:
+            print(f'{Fore.RED}{Style.BRIGHT}Erro: {error}{Fore.RESET}{Style.RESET_ALL}')
+    
+    print()
+
+
+if __name__ == '__main__':
+    main()
+    
